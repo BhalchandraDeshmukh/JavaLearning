a7dfaa003da6b510fb98f2df6a467f0656763a2e
Bhalchandra Deshmukh <bhalchandradeshmukh153@gmail.com>
2022-02-26 16:08:26 -0600

    Added 1 print statement
    Signed-off-by: Bhalchandra Deshmukh <bhalchandradeshmukh153@gmail.com>

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello again....");
+        
+        System.out.println("Added new print statement activity to check github activity");
     }
 }
